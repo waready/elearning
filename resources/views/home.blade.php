c6d97d8def2ad755eb05a0c9aac43ede60c205a7
waready <elbarto119@hotmail.com>
2019-11-21 16:26:04 -0500

    first commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,22 @@
+@extends('layouts.app')
+
+@section('content')
+
+<div class="pl-5 pr-5">
+    <div class="row justify-content-center">
+        @forelse($courses as $course)
+            <div class="col-md-3">
+                @include('partials.courses.card_course')    
+            </div>
+        @empty
+            <div class="alert alert-dark">
+                {{ __("No hay ningún curso disponible") }}
+            </div>
+        @endforelse
+    </div>
+
+    <div class="row justify-content-center">
+        {{ $courses->links() }}
+    </div>
+</div>
+@endsection
